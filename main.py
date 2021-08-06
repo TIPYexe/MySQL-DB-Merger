d8fbc110a414cb0ad607da6ea71b9d39565a1090
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-08-06 10:58:30 +0300

    Update 2 tables + connection table

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import re
 # ex: provider, providerregions
 #   : regions, providerregions
 
-def mergeTabels(db_to_merge, db_main, table1, table2, key_table_1, key_table_2):
+def mergeTabels(db_to_merge, db_main, table1, table2, table3, key_table_1, key_table_2_1, key_table_3, key_table_2_3):
     mydb1 = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -43,19 +43,22 @@ def mergeTabels(db_to_merge, db_main, table1, table2, key_table_1, key_table_2):
     table_2_merge = "`" + db_to_merge + "`.`" + table2 + "`"
     table_1_main = "`" + db_main + "`.`" + table1 + "`"
     table_2_main = "`" + db_main + "`.`" + table2 + "`"
+    table_3_main = "`" + db_main + "`.`" + table3 + "`"
+    table_3_merge = "`" + db_to_merge + "`.`" + table3 + "`"
 
-    sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_1_main, key_table_1, key_table_2)
+    sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_1_main, key_table_1, key_table_2_1, 0, 2)
+    sameIdConflictIssueSolver(mydb1, mydb2, table_3_merge, table_2_merge, table_3_main, key_table_3, key_table_2_3, 0, 1)
     # 0 = id, 2 = type, 0 = skips no columns | 3 = providerId, 4 = regionId, 1 = skips the 1st column
-    mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, 0, 2, 0)
-    mergeTheNewData(mydb1, mydb2, table_2_merge, table_2_main, 3, 4, 1)
+    mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, 0, 2, 0, 0)
+    mergeTheNewData(mydb1, mydb2, table_3_merge, table_3_main, 0, 1, 0, 2)
+    mergeTheNewData(mydb1, mydb2, table_2_merge, table_2_main, 3, 4, 1, 0)
     mydb2.commit()
 
 
 # will assign a new and unique ID to each
-def sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_1_main, key_1, key_2):
+def sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_1_main, key_1, key_2, i, j):
     mycursos_1_merge = mydb1.cursor()
     mycursor_1_main = mydb2.cursor()
-    mycursor_2_main = mydb2.cursor()
 
     mycursos_1_merge.execute("SELECT * FROM " + table_1_merge)
     myresult_1_merge = mycursos_1_merge.fetchall()
@@ -65,20 +68,20 @@ def sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_
 
     # newId_1 = last (hopefully) highest Id from the main DB + 1
     if len(myresult_1_main) > len(myresult_1_merge):
-        newId_1 = myresult_1_main[len(myresult_1_main) - 1][0] + 1
+        newId_1 = myresult_1_main[len(myresult_1_main) - 1][i] + 1
     else:
-        newId_1 = myresult_1_merge[len(myresult_1_merge) - 1][0] + 1
+        newId_1 = myresult_1_merge[len(myresult_1_merge) - 1][i] + 1
 
     # TODO: optimize seaerch
     for index1 in myresult_1_merge:
         for index2 in myresult_1_main:
             # if they have the same ID
-            if index1[0] == index2[0]:
+            if index1[i] == index2[i]:
                 # but they have different values
-                if index1[2] != index2[2]:
+                if index1[j] != index2[j]:
                     # I update manually the Id in all the tables where it appeared
                     # TODO: see if I can extract the connections between tables in order to make the changes recursive
-                    oldId = str(index1[0])
+                    oldId = str(index1[i])
 
                     # print("UPDATE " + table_1_merge + " SET `" + key_1 + "` = '" + str(newId_1) + "' WHERE `" + key_1 + "` = " + oldId + ";")
 
@@ -86,7 +89,7 @@ def sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_
                         newId_1) + " WHERE `" + key_1 + "` = " + oldId + ";")
                     mycursos_1_merge.execute("UPDATE " + table_2_merge + " SET `" + key_2 + "` = " + str(
                         newId_1) + " WHERE `" + key_2 + "` = " + oldId + ";")
-                    # TODO: region
+
                     newId_1 += 1
 
     mydb1.commit()
@@ -94,7 +97,7 @@ def sameIdConflictIssueSolver(mydb1, mydb2, table_1_merge, table_2_merge, table_
 
 # TODO: update in providerregions si providers: set metadata = {} where metadata = null
 
-def mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, i, j, skip):
+def mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, i, j, skip, stop):
     mycursor1 = mydb1.cursor()
     mycursor2 = mydb2.cursor()
 
@@ -111,7 +114,7 @@ def mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, i, j, skip):
         table_1_main.replace('`', '').split('.')[0] + "' ORDER BY ORDINAL_POSITION ASC")
     columnsRaw = mycursor1.fetchall()
     columnNames = list(map(lambda x: re.sub("[(),']", "", str(x)), columnsRaw))
-    columnCommand = ", ".join(columnNames[skip:])
+    columnCommand = ", ".join(columnNames[skip:(len(columnNames)-stop)])
 
     for index1 in myresult1:
         found = False
@@ -126,16 +129,18 @@ def mergeTheNewData(mydb1, mydb2, table_1_merge, table_1_main, i, j, skip):
             # daca merge-uim bd1 in 2. Atunci join(index1) + mycursor2
             # daca merge-uim 2 in 1. Atunci join(index2) + mycursor1
             dataRaw = list(map(lambda x: str(x), index1))
-            dataCommand = "'" + "', '".join(dataRaw[skip:]) + "'"
+            dataCommand = "'" + "', '".join(dataRaw[skip:(len(dataRaw)-stop)]) + "'"
             dataCommand = dataCommand.replace("'None'", "''")
 
             # insert si in tabel_2
-            # print("INSERT INTO " + table_1_main + " (" + columnCommand + ") VALUES (" + dataCommand + ");")
+            print("INSERT INTO " + table_1_main + " (" + columnCommand + ") VALUES (" + dataCommand + ");")
             mycursor2.execute("INSERT INTO " + table_1_main + " (" + columnCommand + ") VALUES (" + dataCommand + ");")
 
     mydb2.commit()
 
 
-# 1 = branch to merge
-# 2 = main
-mergeTabels("urbanair-side", "urbanair-main", "providers", "providerregions", "id", "providerId")
+# 1st = branch to merge (dev, baza de date la care ne conectam noi, pe server)
+# 2nd = main (production de pe slack)
+# table2 = tabel de legatura
+# [1st-db-name, 2nd-db-name, table1, table2, table3, key_table1, key_to_match_key_table1, key_table3, key_to_match_key_table3]
+mergeTabels("urbanair-side", "urbanair-main", "providers", "providerregions", "region", "id", "providerId", "id", "regionId")
